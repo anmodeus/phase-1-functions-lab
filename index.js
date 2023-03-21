6abef68a558be41f2e11576e0ba4b9c97bc6e5e3
Anna Perez <anna.perez.guitar@gmail.com>
2023-03-20 17:11:08 -0700

    adds 4 functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    let n = someValue - 42;
+    return Math.abs(n);
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue)*264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let d = (start - destination)*264;
+    return Math.abs(d);
+}
+
+function calculatesFarePrice(start, destination){
+    let feetCharged = distanceTravelledInFeet(start, destination);
+
+        let fareMessage;
+    
+        if (feetCharged >= 400 && feetCharged<= 2000){
+            fareMessage = (feetCharged - 400)* 0.02;
+        }
+        else if (feetCharged >2000 && feetCharged <=2500){
+            fareMessage = 25;
+        }
+        else if (feetCharged > 2500){
+            fareMessage = 'cannot travel that far';
+        }
+        else {
+            fareMessage = 0;
+        }
+        return fareMessage;
+    }
